66e26cd22a2471ba989bf237f93682b8cc729329
POSTI-25 <jahnavisingh512@gmail.com>
2025-10-17 19:07:02 +0530

    feat: added structs for action condition and rules

--- apps/desktop/src-tauri/src/domain/models.rs
+++ apps/desktop/src-tauri/src/domain/models.rs
@@ -1,16 +1,27 @@
 use serde::{Serialize, Deserialize};
+use uuid::Uuid;
 
 #[derive(Serialize, Deserialize, Debug)]
-
-struct Rule {
-
+pub struct Rule {
+    pub id: Uuid,
+    pub rule_name: String,
+    pub priority: i32,
+    pub conditions: Vec<Condition>,
+    pub action: Action,
+    pub enabled: bool,
 }
 
-struct Condition {
-
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Condition {
+    pub fact: String,
+    pub operator: String,
+    pub value: String 
 }
 
-struct Action {
-
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Action {
+    pub profile: String,
+    pub browser: String,
+    pub url: String
 }
 
