afc689a5347ff71079bd838bc84ed987ecbba6c1
namitha-022 <namitharavikumar22@gmail.com>
2025-10-17 22:18:13 +0530

    added eslint and prettier

---
+++ apps/desktop/.eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', 'src-tauri', 'node_modules', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
